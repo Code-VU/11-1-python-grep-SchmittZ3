43f4c3e4c37bbadbb7c5122fc0269996698d1995
Zach Schmitt <Zach.Schmitt@vu.com>
2022-05-17 17:19:30 -0500

    Schmitt 11.1 Attempt 1

--- grep.py
+++ grep.py
@@ -5,6 +5,18 @@ import sys
 
 def countpatterninfile():
     regular_expression = input("Enter a regular expression: ")
+    x = open('mbox-long.txt')
+    count = 0
+
+    for line in x:
+        if re.search(regular_expression, line):
+            count = count + 1
+
+    print("mbox.txt had",(count),'lines that matched',(regular_expression))
+    
+  #  print('mbox.txt had' (z) 'lines that matched' (regular_expression))
+
+
 
 if __name__ == '__main__':
     # this is called a main method
